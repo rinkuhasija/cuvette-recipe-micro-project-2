40cf379baa083d9e1a8729fb4997db168246d60c
Rinku Hasija <rishabhasija5454@gmail.com>
2023-04-08 20:04:28 +0530

    PORT-env

--- index.js
+++ index.js
@@ -82,6 +82,6 @@ app.post('/:id/delete', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server started on port 3000');
 });
